6ec4df4c00e7a8dbb243805e2f70643c048cde53
Robin legg <ZekeLegg@Robins-MacBook-Air-2.local>
2017-06-26 16:28:47 -0400

    recieving error message for division by zero

--- division/division.rb
+++ division/division.rb
@@ -1,7 +1,9 @@
 def dividing(r , s)
-	r / s
-	#if s == 0
-		#puts "this problem cannot be solved"
+	if s == 0
+	puts "this problem cannot be solved due to division by zero"
+	else
+		r / s
 end
+	end
 
 puts dividing(7 , 0)
